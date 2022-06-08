3737d0fc39a77337facf59263c6ba044b44d0b06
jpmazel <jpmazel@gmail.com>
2022-06-08 17:38:17 +0200

    correction erreur lecteur youtube The service worker navigation preload request was cancelled before 'preloadResponse' settled

--- src/components/Feed/FeedPostVideo.js
+++ src/components/Feed/FeedPostVideo.js
@@ -25,7 +25,7 @@ const FeedPostVideo = ({ videoPost }) => {
         <iframe
           width="300"
           height="170"
-          src={`https://www.youtube.com/embed/${idVideo}`}
+          src={`https://www.youtube-nocookie.com/embed/${idVideo}`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
