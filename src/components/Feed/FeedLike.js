0b0cb083eb12de121ea5e2eb6eed6c918aac9762
jpmazel <jpmazel@gmail.com>
2022-06-20 14:34:06 +0200

    refactor: FeedLike requête customm hook http

--- src/components/Feed/FeedLike.js
+++ src/components/Feed/FeedLike.js
@@ -1,161 +1,92 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./FeedLike.module.css";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import useHttp from "../../hooks/use-http";
 
 const FeedLike = ({ token, idPostsUser, userIdToken }) => {
   const [like, setLike] = useState(null);
   const [reload, setReload] = useState(false);
-
-  const fetchGetLikeHandler = useCallback(async () => {
-    //Aller chercher tous les likes  de la base de données qui sont la table likes_user
-    const url = `${process.env.REACT_APP_API_URL}/api/posts/likes/${idPostsUser}?userId=${userIdToken}`;
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      const dataResponse = await response.json();
-
-      if (response.ok) {
-        setLike(dataResponse.results);
-      } else {
-        console.log("-->fetchGetLikeHandler response PAS ok");
-        throw new Error(dataResponse.error);
-      }
-    } catch (error) {
-      console.log("-->Dans le catch requête fetchGetLikeHandler");
-      console.log(error);
-    }
-  }, [idPostsUser, token, userIdToken]);
+  const { sendRequest: fetchPUTLikeHandler } = useHttp();
+  const { sendRequest: fetchGetLikeHandler } = useHttp();
+  const { sendRequest: fetchPOSTLikeHandler } = useHttp();
 
   //Quand je clique sur le bouton du like du post (le bouton j'aime est à 0 ou neutre - pouce vide-coeur vide)
   const neutralLikeHandler = () => {
     if (like && like.length === 0) {
-      //La requête POST avec fetch pour pour créer la ligne du like dans la table likes_user
-      const fetchPOSTLikeHandler = async () => {
-        const url = `${process.env.REACT_APP_API_URL}/api/posts/likes/?userId=${userIdToken}`;
-
-        const data = {
-          likes_user_id_posts: idPostsUser,
-          likes_user_userId: userIdToken,
-          likes_user_like: 1,
-        };
-        try {
-          //La requête POST avec text (pas d'image)
-          const response = await fetch(url, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(data),
-          });
-
-          //Convertir la reponse du serveur avec la méthode json()
-          const dataResponse = await response.json();
-
-          //Si la response du serveur est OK
-          if (response.ok) {
-            setReload((prevState) => !prevState);
-          } else {
-            console.log("--->fetchPOSTLikeHandler dataResponse PAS OK");
-            console.log(dataResponse);
-          }
-        } catch (error) {
-          console.log("--->Dans le CATCH fetchPOSTLikeHandler");
-          throw new Error(error);
-        }
+      const data = {
+        likes_user_id_posts: idPostsUser,
+        likes_user_userId: userIdToken,
+        likes_user_like: 1,
       };
 
-      fetchPOSTLikeHandler();
+      const resquestConfig = {
+        url: `http://localhost:3000/api/posts/likes/?userId=${userIdToken}`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: data,
+      };
+
+      fetchPOSTLikeHandler(resquestConfig, () =>
+        setReload((prevState) => !prevState)
+      );
     } else if (like && like[0]) {
-      //La requête PUT  pour faire passer le like de 1 a 0 ou de 0 à 1
-      const fetchPUTLikeHandler = async () => {
-        const url = `${process.env.REACT_APP_API_URL}/api/posts/likes/${like[0].id_likes_user}?userId=${userIdToken}`;
-
-        const data = {
-          likes_user_like: like[0].likes_user_like,
-        };
-
-        try {
-          //La requête POST avec text (pas d'image)
-          const response = await fetch(url, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(data),
-          });
-
-          //Convertir la reponse du serveur avec la méthode json()
-          const dataResponse = await response.json();
-
-          //Si la response du serveur est OK
-          if (response.ok) {
-            setReload((prevState) => !prevState);
-          } else {
-            console.log("--->fetchPUTLikeHandler dataResponse PAS OK");
-            console.log(dataResponse);
-          }
-        } catch (error) {
-          console.log("--->Dans le CATCH fetchPUTLikeHandler");
-          throw new Error(error);
-        }
+      const data = {
+        likes_user_like: like[0].likes_user_like,
       };
-      fetchPUTLikeHandler();
+
+      const requestConfig = {
+        url: `http://localhost:3000/api/posts/likes/${like[0].id_likes_user}?userId=${userIdToken}`,
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: data,
+      };
+
+      fetchPUTLikeHandler(requestConfig, () =>
+        setReload((prevState) => !prevState)
+      );
     }
   };
 
   //Lorsque la ligne like existe sur la bdd passer le like de 1 à 0 ou de 0 à 1
   const likeHandler = () => {
-    //La requête PUT  pour faire passer le like de 1 a 0 ou de 0 à 1
-    const fetchPUTLikeHandler = async () => {
-      const url = `${process.env.REACT_APP_API_URL}/api/posts/likes/${like[0].id_likes_user}?userId=${userIdToken}`;
-
-      const data = {
-        likes_user_like: like[0].likes_user_like,
-      };
-
-      try {
-        //La requête POST avec text (pas d'image)
-        const response = await fetch(url, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(data),
-        });
-
-        //Convertir la reponse du serveur avec la méthode json()
-        const dataResponse = await response.json();
-
-        //Si la response du serveur est OK
-        if (response.ok) {
-          setReload((prevState) => !prevState);
-        } else {
-          console.log(
-            "--->fetchPUTLikeHandler - likeHandler- dataResponse PAS OK"
-          );
-          console.log(dataResponse);
-        }
-      } catch (error) {
-        console.log("--->Dans le CATCH fetchPUTLikeHandler - likeHandler");
-        throw new Error(error);
-      }
+    const data = {
+      likes_user_like: like[0].likes_user_like,
     };
 
-    fetchPUTLikeHandler();
+    const requestConfig = {
+      url: `http://localhost:3000/api/posts/likes/${like[0].id_likes_user}?userId=${userIdToken}`,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: data,
+    };
+
+    fetchPUTLikeHandler(requestConfig, () =>
+      setReload((prevState) => !prevState)
+    );
   };
 
   useEffect(() => {
-    fetchGetLikeHandler();
-  }, [reload, fetchGetLikeHandler]);
+    //objet de configuration
+    const sendRequest = {
+      url: `http://localhost:3000/api/posts/likes/${idPostsUser}?userId=${userIdToken}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    fetchGetLikeHandler(sendRequest, (dataResponse) => setLike(dataResponse));
+  }, [reload, fetchGetLikeHandler, idPostsUser, token, userIdToken]);
 
   return (
     <div className={classes.feedLike}>
@@ -168,6 +99,7 @@ const FeedLike = ({ token, idPostsUser, userIdToken }) => {
         </p>
       ) : (
         <p onClick={neutralLikeHandler}>
+          {" "}
           <ThumbUpOutlinedIcon fontSize="large" />
         </p>
       )}
